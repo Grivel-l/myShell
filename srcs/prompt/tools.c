3491b3b506cbc4701ef374f15ec7333e706cd5b2
Leo Grivel <legrivel@student.le-101.fr>
2018-03-18 00:08:13 +0100

    Multiline editing working

--- srcs/prompt/tools.c
+++ srcs/prompt/tools.c
@@ -6,7 +6,7 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/02/11 20:46:07 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/17 23:15:35 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/18 00:07:04 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -25,34 +25,6 @@ int			write_line(t_prompt *prompt)
 		return (rewind_cursor(prompt, length));
 	}
 	return (0);
-}
-
-static size_t	to_start_length(t_prompt *prompt)
-{
-	char	*tmp;
-	char	*str;
-	size_t	length;
-	size_t	pos_dup;
-
-	tmp = NULL;
-	if (prompt->line == NULL && prompt->commands == NULL)
-		return (0);
-	if (prompt->line != NULL && prompt->commands != NULL && prompt->commands->content != NULL)
-		if ((tmp = ft_strjoin(prompt->commands->content, prompt->line)) == NULL)
-			return (-1);
-	if (prompt->line == NULL)
-		str = prompt->commands->content;
-	else
-		str = tmp == NULL ? prompt->line : tmp;
-	length = 0;
-	pos_dup = prompt->pos;
-	while (pos_dup > 0)
-	{
-		length += 1;
-		pos_dup -= 1;
-	}
-	free(tmp);
-	return (length);
 }
 
 size_t		to_line_start_length(t_prompt *prompt, size_t col)
@@ -88,9 +60,15 @@ int			clear_all(t_prompt *prompt)
 	size_t	old_pos;
 
 	old_pos = prompt->pos;
-	prompt->extra = PL;
-	if (rewind_cursor(prompt, to_start_length(prompt)) == -1)
+	if (rewind_cursor(prompt, prompt->pos) == -1)
 		return (-1);
+	prompt->extra = PL;
+	while (prompt->extra > 0)
+	{
+		if (put_cap("le") == -1)
+			return (-1);
+		prompt->extra -= 1;
+	}
 	if (put_cap("cd") == -1)
 		return (-1);
 	if (prompt->quoting || isquoting(prompt->commands))
