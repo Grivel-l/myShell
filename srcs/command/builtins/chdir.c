e1ba35d47c48d207f934b607fdd845c32767a516
Leo Grivel <legrivel@student.le-101.fr>
2018-03-24 01:26:38 +0100

    Added - and -- to chdir

--- srcs/command/builtins/chdir.c
+++ srcs/command/builtins/chdir.c
@@ -6,7 +6,7 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/03/21 01:29:39 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/21 01:30:20 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/24 01:25:41 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -65,17 +65,34 @@ static int	update_pwd(char *pwd, char ***environ)
 	return (create_pwd(environ));
 }
 
+static char	*get_dir(char *arg, char **environ)
+{
+	if (arg == NULL ||
+			(arg != NULL && ft_strcmp(arg, "--") == 0))
+		return (get_myenv("HOME", environ));
+	else if (ft_strcmp(arg, "-") == 0)
+		return (get_myenv("OLDPWD", environ));
+	else
+		return (arg);
+}
+
 int			cd_builtin(t_command *cmd)
 {
 	char	*dir;
 	char	*pwd;
 
 	cmd->cmd_ret = 0;
-	dir = cmd->args[1] == NULL ? get_myenv("HOME", cmd->environ) : cmd->args[1];
+	dir = get_dir(cmd->args[1], cmd->environ);
 	if (dir == NULL)
 	{
-		env_enoent("cd", "HOME");
+		env_enoent("cd", (cmd->args[1] != NULL &&
+					ft_strcmp(cmd->args[1], "-")) == 0 ? "OLDPWD" : "HOME");
 		return (0);
+	}
+	if (cmd->args[1] != NULL && ft_strcmp(cmd->args[1], "-") == 0)
+	{
+		ft_putstr(dir);
+		ft_putchar('\n');
 	}
 	if (chdir(dir) == -1)
 		return (check_errors(cmd, dir));
